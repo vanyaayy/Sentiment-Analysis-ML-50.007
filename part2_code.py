b1eab70f4e1558a846013f488bec49b5d61462ca
vanyajalan <vanyajalan02@gmail.com>
2023-08-09 20:49:45 +0800

    part 2 update

--- part2_code.py
+++ part2_code.py
@@ -40,58 +40,92 @@ def estimate_transition_parameters(train_path):
         transition_parameters[tag1][tag2] = count / tag_count[tag1]
     return transition_parameters
 
-def viterbi_algorithm(emission_parameters, transition_parameters, dev_in_path, dev_out_path):
-    output_lines = []
-    sentence_tokens = []
-    with open(dev_in_path, 'r', encoding='utf-8') as file:
+def viterbi(emission_parameters, transition_parameters, sentence):
+    words = sentence.split()
+    n = len(words)
+    
+    viterbi_matrix = defaultdict(lambda: defaultdict(float))
+    backpointers = defaultdict(dict)
+    
+    viterbi_matrix[0]['START'] = 1.0
+    
+    for k in range(1, n+1):
+        word = words[k-1]
+        if word not in emission_parameters:
+            word = '#UNK#'
+        for v in emission_parameters[word]:
+            for u in viterbi_matrix[k-1]:
+                score = viterbi_matrix[k-1][u] * transition_parameters[u][v] * emission_parameters[word][v]
+                if score > viterbi_matrix[k][v]:
+                    viterbi_matrix[k][v] = score
+                    backpointers[k][v] = u
+    
+    opt_tags = []
+    max_score = 0.0
+    last_tag = ''
+    
+    for tag in viterbi_matrix[n]:
+        if viterbi_matrix[n][tag] > max_score:
+            max_score = viterbi_matrix[n][tag]
+            last_tag = tag
+    
+    if last_tag != '':
+        opt_tags.append(last_tag)
+        for k in range(n, 1, -1):
+            opt_tags.append(backpointers[k][last_tag])
+            last_tag = backpointers[k][last_tag]
+        
+        opt_tags.reverse()
+        return opt_tags
+    
+    else:
+        default = []
+        for i in range (0,n) :
+            default.append('O')
+        return default
+    
+
+###RU#####
+train_path = "Data/RU/train"
+dev_in_path = "Data/RU/dev.in"
+dev_predicted_path = "Data/RU/dev.p2.out"
+    
+emission_params = estimate_emission_parameters(train_path)
+transition_params = estimate_transition_parameters(train_path)
+
+sentences = ""   
+with open(dev_in_path, 'r', encoding='utf-8') as file:
+    with open(dev_predicted_path, 'w', encoding='utf-8') as file_write:
         for line in file:
-            line = line.strip()
-            if line:
-                sentence_tokens.append(line)
+            word = line.strip()
+            if word:
+                sentences+= word + " "
             else:
-                n = len(sentence_tokens)
-                pi = defaultdict(lambda: defaultdict(float))
-                backpointer = defaultdict(lambda: defaultdict(str))
-                pi[0]['START'] = 1
-                for k in range(1, n + 1):
-                    token = sentence_tokens[k - 1]
-                    if token not in emission_parameters:
-                        token = '#UNK#'
-                    for v in emission_parameters[token]:
-                        max_prob = 0
-                        max_u = ''
-                        for u in transition_parameters:
-                            prob = pi[k - 1][u] * transition_parameters[u][v] * emission_parameters[token][v]
-                            if prob > max_prob:
-                                max_prob = prob
-                                max_u = u
-                        pi[k][v] = max_prob
-                        backpointer[k][v] = max_u
-                max_prob = 0
-                max_u = ''
-                for u in transition_parameters:
-                    prob = pi[n][u] * transition_parameters[u]['STOP']
-                    if prob > max_prob:
-                        max_prob = prob
-                        max_u = u
-                backpointer[n + 1]['STOP'] = max_u
-                best_tags = ['STOP']
-                for k in range(n + 1, 1, -1):
-                    best_tags.append(backpointer[k][best_tags[-1]])
-                best_tags = best_tags[::-1][1:]
-                for token, tag in zip(sentence_tokens, best_tags):
-                    output_lines.append(f"{token} {tag}")
-                output_lines.append("")
-                sentence_tokens = []
-    with open(dev_out_path, 'w', encoding='utf-8') as file:
-        file.write('\n'.join(output_lines))
+                    predicted_tags = viterbi(emission_params, transition_params, sentences)
+                    for word, tag in zip(sentences.split(), predicted_tags):
+                        file_write.write(f"{word} {tag}\n")
+                    file_write.write("\n")
+                    sentences=""
 
-###ES####
-emission_params_ES = estimate_emission_parameters("Data/ES/train")
-transition_params_ES = estimate_transition_parameters("Data/ES/train")
-viterbi_algorithm(emission_params_ES, transition_params_ES, "Data/ES/dev.in", "Data/ES/dev.p2.out")
 
-###RU####
-emission_params_RU = estimate_emission_parameters("Data/RU/train")
-transition_params_RU = estimate_transition_parameters("Data/RU/train")
-viterbi_algorithm(emission_params_RU, transition_params_RU, "Data/RU/dev.in", "Data/RU/dev.p2.out")
+###ES#####
+train_path = "Data/ES/train"
+dev_in_path = "Data/ES/dev.in"
+dev_predicted_path = "Data/ES/dev.p2.out"
+    
+emission_params = estimate_emission_parameters(train_path)
+transition_params = estimate_transition_parameters(train_path)
+
+sentences = ""   
+with open(dev_in_path, 'r', encoding='utf-8') as file:
+    with open(dev_predicted_path, 'w', encoding='utf-8') as file_write:
+        for line in file:
+            word = line.strip()
+            if word:
+                sentences+= word + " "
+            else:
+                    predicted_tags = viterbi(emission_params, transition_params, sentences)
+                    for word, tag in zip(sentences.split(), predicted_tags):
+                        file_write.write(f"{word} {tag}\n")
+                    file_write.write("\n")
+                    sentences=""
